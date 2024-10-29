336d47c9ece429e9444a42e25e8bc83c7479e227
Gabriella Weis <glweis@willamette.edu>
2024-10-28 20:00:22 -0700

    Update Prob2.py

--- Prob2.py
+++ Prob2.py
@@ -5,30 +5,30 @@
 ##################################################
 
 def is_magic_square(array:list[list[int]]) -> bool:
-    n = len(small)
-    for row in small:
+    n = len(array)
+    for row in array:
         if n == 0 or len(row) != n:
             return False
         
     # sum that all other rows, columns, and diagonals must match
-    magic_sum = sum(small[0])
+    magic_sum = sum(array[0])
 
     # rows
-    for row in small:
+    for row in array:
         if sum(row) != magic_sum:
             return False
     
     # columns
     for col in range(n):
-        if sum(small[row][col] for row in range(n)) != magic_sum:
+        if sum(array[row][col] for row in range(n)) != magic_sum:
             return False
             
     # left diagonal
-    if sum(small[i][i] for i in range(n)) != magic_sum:
+    if sum(array[i][i] for i in range(n)) != magic_sum:
         return False
 
     # right diagonal
-    if sum(small[i][n - 1 - i] for i in range(n)) != magic_sum:
+    if sum(array[i][n - 1 - i] for i in range(n)) != magic_sum:
         return False
     
     # no conditions are met
@@ -36,3 +36,4 @@ def is_magic_square(array:list[list[int]]) -> bool:
 
 small = [[8,1,6],[3,5,7],[4,9,2]]
 print(is_magic_square(small))
+
